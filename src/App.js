c1111ebecaf63d54c7e0c22bd7b4714f5b121729
parkjoonhwan <ikwladu2@muplus.co.kr>
2023-12-19 15:33:42 +0900

    일기 더미데이터 날짜 최신화

--- src/App.js
+++ src/App.js
@@ -38,31 +38,31 @@ const dummyData = [
         id: 1,
         emotion: 1,
         content: '오늘의 일기 1번',
-        date: 1680674875436,
+        date: 1702962752346,
     },
     {
         id: 2,
         emotion: 2,
         content: '오늘의 일기 2번',
-        date: 1680674895924,
+        date: 1702962799825,
     },
     {
         id: 3,
         emotion: 3,
         content: '오늘의 일기 3번',
-        date: 1680674899007,
+        date: 1702962803601,
     },
     {
         id: 4,
         emotion: 4,
         content: '오늘의 일기 4번',
-        date: 1680674903486,
+        date: 1702962807127,
     },
     {
         id: 5,
         emotion: 5,
         content: '오늘의 일기 5번',
-        date: 1680674908764,
+        date: 1702962810265,
     },
 ];
 
